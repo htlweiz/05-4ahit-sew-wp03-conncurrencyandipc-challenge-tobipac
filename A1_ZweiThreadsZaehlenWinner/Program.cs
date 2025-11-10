58f76bf654bc05ed3779f733bb472a332335f705
tobipac <tobipa@gmx.at>
2025-11-10 08:01:48 +0100

    Übung 1

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -10,17 +10,25 @@ class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
+        Thread threadA = new Thread(() => CountUpThreadA());
+        Thread threadB = new Thread(() => CountDownThreadB());
         
     }
     
     private static void CountUpThreadA()
     {
+        for(int i = 1; i<= 100; i++)
+        {
+            Thread.Sleep(100);
+        }
         
     }
     
     private static void CountDownThreadB()
     {
-       
+        for(int i = 100; i>= 1; i--)
+        {
+            Thread.Sleep(100);
+        }
     }
 }
